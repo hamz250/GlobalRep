ea06e23e64ebc3d75960b53bd3040aabbe2e5c27
Honzo <hamza.bouzidi1@gmail.com>
2014-11-01 08:39:33 +0100

    Personne Added

---
+++ MavenTestProject/src/main/java/com/Gl4/hamza/MavenTestProject/Personne.java
@@ -0,0 +1,16 @@
+package com.Gl4.hamza.MavenTestProject;
+
+public class Personne {
+
+	private String nom;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+}
