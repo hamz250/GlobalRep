ca35ce776b1b760a141de1e84100945d9d2c0822
Honzo <hamza.bouzidi1@gmail.com>
2014-11-01 08:40:29 +0100

    Personne Edited

--- MavenTestProject/src/main/java/com/Gl4/hamza/MavenTestProject/Personne.java
+++ MavenTestProject/src/main/java/com/Gl4/hamza/MavenTestProject/Personne.java
@@ -3,6 +3,9 @@ package com.Gl4.hamza.MavenTestProject;
 public class Personne {
 
 	private String nom;
+	private String prenom;
+	
+	
 
 	public String getNom() {
 		return nom;
@@ -10,6 +13,14 @@ public class Personne {
 
 	public void setNom(String nom) {
 		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
 	}
 	
 	
